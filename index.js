6411ce3586394ecb0c403628fe05e3f28df12151
SazumiVicky <vicihidayat722@gmail.com>
2024-04-01 02:01:03 -0700

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { remote } = require('webdriverio');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 const options = {
     capabilities: {
